894a0420c4d28acf94328152e114972344d0c48e
Andronick Martusheff <71955336+martusheff@users.noreply.github.com>
2020-12-17 19:35:58 -0800

    Create bstConstruction.py
    first attempt

---
+++ bstConstruction.py
@@ -0,0 +1,16 @@
+class BST:
+	def __init__(self, value):
+		self.value = value
+		self.left = None
+		self.right = None
+
+	def insert(self, value):
+		if value < self.value
+		
+
+
+	def contains(self, vlaue):
+
+		pass
+
+	def remove(self, value):
